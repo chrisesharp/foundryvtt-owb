436877896c21cb462abca41cd1325288f8454b87
C. E. Sharp <chrisesharp@gmail.com>
2021-01-02 13:46:31 +0000

    add reload macro

---
+++ src/reload.js
@@ -0,0 +1,21 @@
+function getAmmo(token) {
+    return token.actor.data.items.find(f => f.name.includes("Ammunition") && f.type === "item");
+}
+function reloadAmmo(token, ammo) {
+    ammo.data.quantity.value = Math.max(0,ammo.data.quantity.max);
+    token.actor.updateEmbeddedEntity("OwnedItem", {...ammo});
+}
+
+if (token) {
+    const ammo = getAmmo(token);
+    const emptyAmmo = (ammo && ammo.data.quantity.value === 0);
+    if (emptyAmmo) {
+        reloadAmmo(token, ammo);
+        var messageContent = `Reloaded ${ammo.name}`;
+        var chatData = {
+            user: game.user._id,
+            speaker: ChatMessage.getSpeaker(),
+            content: messageContent};
+        ChatMessage.create(chatData, {});
+    }
+}
